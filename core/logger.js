4e82590c5171fc2e7daec37604e060040f85df67
Ryan Megidov <ryan@megidov.com>
2016-11-03 08:33:02 +0100

    Require logger only once

--- core/logger.js
+++ core/logger.js
@@ -1,9 +1,9 @@
 // Enable logging
 const appName = 'app'
 const enabled = `${appName}:*,server:*,webpack:*`
+const logger = require('debug')
 
 if (process.env.BROWSER) {
-    const logger = require('debug')
     // Enable logging
     logger.enable(enabled)
 } else {
@@ -13,7 +13,6 @@ if (process.env.BROWSER) {
     if (process.env.NODE_ENV !== 'production') {
         // Let's add some colors to our console.
         // You might want to use your own logger here.
-        const logger  = require('debug')
         console.debug = logger(appName + ':debug').bind(console);
         console.info  = logger(appName + ':info').bind(console);
         console.warn  = logger(appName + ':warn').bind(console);
